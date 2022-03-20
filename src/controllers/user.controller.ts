603ad7bcbe94abbdbde2ca8d50365d44ee901488
atk81 <ashutoshkushwaha81@gmail.com>
2022-03-20 23:15:18 +0530

    Signup/verify routes

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,10 +1,15 @@
 import { NextFunction, Request, Response } from 'express';
-import { CustomError } from '../utils/response/error';
-import User, { IUser } from '../models/user.model';
-import { cookieToken } from '../utils/cookieToken';
+import fs from "fs";
+import jwt from "jsonwebtoken";
 import mongoose from 'mongoose';
+import path from 'path';
 import { Cloudinary } from '../config/cloudinary.config';
 import { Nodemailer } from '../config/nodemailer.config';
+import User, { IUser } from '../models/user.model';
+import { cookieToken } from '../utils/cookieToken';
+import { CustomError } from '../utils/response/error';
+const pathToPublicKey = path.join(__dirname, '..', '..', '.public.key.pem');
+const publicKey = fs.readFileSync(pathToPublicKey, 'utf8');
 
 export const signup = async (req: Request, res: Response, next: NextFunction) => {
     // Get the user info from the request body
@@ -62,5 +67,42 @@ export const signup = async (req: Request, res: Response, next: NextFunction) =>
             const error = new CustomError(500, "Application", "Error creating user", err);
             next(error);
         }
+    }
+}
+
+export const signupVerify = async (req: Request, res: Response, next: NextFunction) => {
+    const token = req.params.token;
+    if(!token) {
+        const err = new CustomError(400, "General", "Token is required", null);
+        return next(err);
+    }
+
+    try{
+        // Verify token using JWT
+        const decode = jwt.verify(token,publicKey);
+        // Check for expiration
+        if(decode.exp < Date.now()) {
+            const err = new CustomError(400, "General", "Token has expired", null);
+            return next(err);
+        } else {
+            // Find the user
+            const user = await User.findById(decode.sub);
+            if(!user) {
+                const err = new CustomError(400, "General", "User not found", null);
+                return next(err);
+            } else if(user.status === "active") {
+                const err = new CustomError(400, "General", "User already verified", null);
+                return next(err);
+            }
+            // Update the user status
+            await user.updateOne({ status: "active", confirmEmailToken: null });
+            // Hide the password
+            user.password = null;
+            // Generate the token
+            cookieToken(user, res);
+        }
+    } catch(err){
+        const error = new CustomError(404, "General", "Token is invalid", err);
+        return next(error);
     }
 }
